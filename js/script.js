329b0bc2cf6af4a831a49b136598dfd2d9c2be30
Richard Nadji <richardnadji@hotmail.com>
2017-06-05 03:40:51 +0200

    add guess number game to script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var min = 0;
-var max = 10000;
+var max = 10;
 var randomNumber = getRandomNumber(min, max);
 var guess;
 var attempts = 0;
+var correctGuess = false;
 
 function getRandomNumber(min, max) {
   return Math.floor( Math.random() * (max - min + 1) + min);
@@ -15,7 +16,7 @@ while ( counter < 10 ) {
   document.write(random + ' ');
   counter += 1;
 }
-*/
+
 
 while ( guess !== randomNumber ) {
   guess = getRandomNumber(min, max);
@@ -23,3 +24,14 @@ while ( guess !== randomNumber ) {
 }
 document.write('<p>The random number was: ' + randomNumber + '</p>');
 document.write('<p>It took this computer to ' + attempts + ' attempts to guess this number</p>');
+*/
+
+do {
+  guess = prompt('Guess the secret number between 0 and 10 ' + randomNumber);
+  attempts += 1;
+  if ( parseInt(guess) === randomNumber ) {
+    correctGuess = !correctGuess;
+  }
+} while ( !correctGuess )
+document.write('<p>The secret number was: ' + randomNumber + '</p>');
+document.write('<p>You needed ' + attempts + ' attempt(s) to guess the number</p>');
